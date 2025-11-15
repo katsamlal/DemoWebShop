753e898998bc1f87576b8570df2d431c5bd51699
katsamlal <samlal.katrina@gmail.com>
2025-11-14 23:17:34 -0500

    initialized elements on loginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,10 +2,15 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import utils.CommonMethods;
 
 public class LoginPage extends CommonMethods {
 
+    public LoginPage() {
+        PageFactory.initElements(driver, this);
+    }
+    
     @FindBy(id="Email")
     public WebElement emailField;
 
